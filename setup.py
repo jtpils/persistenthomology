6c295c771d3ae368f8ff5fb935969b2c2b91b98d
Charles Siegel <charles.m.siegel@gmail.com>
2019-05-23 12:36:50 -0700

    feat: Initialize requirements, setup and module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='persistenthomology',
+    version='0.0.1',
+    packages=['persistenthomology'],
+    url='https://github.com/charlesmsiegel/persistenthomology',
+    license='Apache License 2.0',
+    author='Charles Siegel',
+    author_email='charles.m.siegel@gmail.com',
+    description='A library for computing the persistent homology of point clouds'
+)
